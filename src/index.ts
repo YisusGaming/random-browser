3ff53b4192e2a17b412310ba1cf2074b13b3b10c
Yisus <jesusgameroom@gmail.com>
2022-12-09 16:28:23 -0400

    public setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
-let main;
+const publicPath = path.join(__dirname, 'public');
+
+let main : BrowserWindow;
 app.on('ready', () => {
     main = new BrowserWindow({
         title: 'Random Browser - Loading...',
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
-        }
+        },
+        show: false
     });
+    main.loadFile(path.join(publicPath, 'index.html'));
+    main.on('ready-to-show', () => main.show());
 });
