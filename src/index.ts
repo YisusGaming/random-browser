a4c1317860abab37144b5946b478fb7468d0ef6e
Yisus <jesusgameroom@gmail.com>
2022-12-09 15:45:01 -0400

    Added electron

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hi mom!');
+import { app, BrowserWindow } from 'electron';
+
+let main;
+app.on('ready', () => {
+    main = new BrowserWindow({
+        title: 'Random Browser - Loading...',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+});
