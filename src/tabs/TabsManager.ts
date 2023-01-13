d14575cdddd6ed5aa410ae913c83ff9e6ef0b2c5
Yisus <jesusgameroom@gmail.com>
2023-01-13 14:04:12 -0400

    Revert "working on new tab system"

--- src/tabs/TabsManager.ts
+++
@@ -1,7 +0,0 @@
-import BrowserTab from "./BrowserTab.js";
-
-export default class TabsManager {
-    static createTab(url: string, title?: string) : BrowserTab {
-        return new BrowserTab(url);
-    }
-}
