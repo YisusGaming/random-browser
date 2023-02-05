6d961cbef1a2486f7df429d79783ec8bbf70ab9d
Yisus <jesusgameroom@gmail.com>
2023-02-05 10:57:36 -0400

    auto generate the tab id

--- src/tabs/TabManager.ts
+++ src/tabs/TabManager.ts
@@ -8,11 +8,16 @@ class TabManager {
         this.#activeTab = 0;
     }
 
-    createNewTab(url: string, id: number) : BrowserTab {
-        let tab = new BrowserTab(url, id);
+    createNewTab(url: string) : BrowserTab {
+        let id = this.#tabs.length - 1;
+        let tab = new BrowserTab(url, id >= 0 ? id : 0);
         this.#tabs.push(tab);
 
         return tab;
+    }
+
+    updateActiveTab(tabId: number) {
+        this.#activeTab = tabId;
     }
 }
 
