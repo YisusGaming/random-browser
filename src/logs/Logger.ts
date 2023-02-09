47ca94fc3e080db27f6ab0abcddc038b1b3e4ad6
Yisus <jesusgameroom@gmail.com>
2023-02-09 17:18:24 -0400

    well documented logger

--- src/logs/Logger.ts
+++ src/logs/Logger.ts
@@ -1,24 +1,36 @@
+/**
+ * The logger class.
+ * @since v2.0.0
+ * 
+ * Prints messages to `stdout` and `stderr`.
+ */
 export default class Logger {
     private browserVersion: string;
 
     /**
-     * The logger class.
-     *
-     * Use this print messages to stdout and stderr.
-     * @param browserVersion Random Browser's current version allocated in app.json
+     * @param browserVersion Random Browser's current version allocated in `app.json`
      */
     constructor(browserVersion: string) {
         this.browserVersion = browserVersion;
     }
 
+    /**
+     * Prints a message to the `stdout`.
+     */
     logMessage(msg: string): void {
         console.log(`[LOG:${this.browserVersion}] => ${msg}`);
     }
 
+    /**
+     * Prints a warning to the `stderr`.
+     */
     logWarning(warn: string): void {
         console.warn(`[WARNING!] => ${warn} | ${this.browserVersion}`);
     }
 
+    /**
+     * Prints an string or an Error to stderr.
+     */
     logError(err: string | Error, origin: { file: string; line: number }): void {
         if (typeof err == 'string') {
             console.error(
