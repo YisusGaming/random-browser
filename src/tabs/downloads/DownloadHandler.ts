971cf4acde68e7ab26db2c04a63c7be317b64338
Yisus <jesusgameroom@gmail.com>
2023-04-29 17:03:37 -0400

    handling downloads with logs

--- src/tabs/downloads/DownloadHandler.ts
+++ src/tabs/downloads/DownloadHandler.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, DownloadItem, Event, WebContents } from "electron";
+import { logger } from "../../index.js";
 
 class DownloadHandler {
     /**
@@ -6,7 +7,26 @@ class DownloadHandler {
      * this function should be called in a `will-download` electron event.
      */
     public handleDownload(event: Event, item: DownloadItem, webContents: WebContents, tab: BrowserWindow): void {
+        logger.logMessage(`Download triggered.`);
+        item.on('updated', (event, state) => {
+            if (state == 'interrupted') {
+                logger.logWarning("Donwload interrupted but can be resumed.");
+            } else if (state == 'progressing') {
+                if (item.isPaused()) {
+                    logger.logMessage("Download paused.");
+                } else {
+                    logger.logMessage(`Download progress:\n${item.getReceivedBytes()} bytes out of ${item.getTotalBytes()}`);
+                }
+            }
+        });
 
+        item.once('done', (event, state) => {
+            if (state == 'completed') {
+                logger.logMessage(`Download completed succesfully.`);
+            } else {
+                logger.logWarning(`Download failed: ${state}`);
+            }
+        });
     }
 }
 
